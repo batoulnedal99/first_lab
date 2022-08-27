cff7751933f20e475f6bcab261f64fc8d6d44b6d
batoulnedal99 <bnaldragma17@cit.just.edu.jo>
2022-08-27 20:52:35 +0300

    informations updated

--- main.dart
+++ main.dart
@@ -1,6 +1,17 @@
 void main()
 {
 
-String myName="Batoul";
+Map batoulInformations = {
 
+"firstName":"Batoul",
+"lastName":"Al-Daraghma",
+"age":23,
+"nationality":"Jordanian",
+"favColor":"Green",
+"famlyMembers":[ { "father":"Nedal","mother":"Noha","sister":"Lama","brothers":["Ammar","Obada","Belal","Omar"] } ],
+"educationInormations":{ "name":"JUST","faculty":"IT","specialization":"Computer Engineering","year":2022 }
+};
+print(batoulInformations);
 }
+
+ 
